38af3672f299f07c0be729eafb8e5d0c4972b0db
acfive <128711834+acfive@users.noreply.github.com>
2023-10-24 21:32:37 -0400

    Create connectdatabase.py

---
+++ connectdatabase.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+
+#TO DO 
+#REPLACE USERNAME, PASSWORD AND DATABASE NAME WITH ORIGINAL CREATOR 
+
+cnx = mysql.connector.connect(user='username', password='password',
+                              host='127.0.0.1',
+                              database='employees')
+cnx.close()
