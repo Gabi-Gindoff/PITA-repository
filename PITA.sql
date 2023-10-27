0c02a3a9f9bf0746ac10415cb170a92d9736ee68
katherinerbanis <145159003+katherinerbanis@users.noreply.github.com>
2023-10-27 13:06:56 -0400

    Update PITA.sql

--- PITA.sql
+++ PITA.sql
@@ -1,9 +1,8 @@
 create table Account (
-psuID varchar(7) not null,
+email varchar(25),
 firstName varchar(25),
 lastName varchar(25),
-email varchar(25),
-primary key (PSUID));
+primary key (email));
 
 create table Chat (
 chatID int not null,
@@ -15,24 +14,26 @@ create table Message (
 messageID int not null,
 text varchar(255) not null,
 timestamp timestamp not null,
-psuID varchar(7) not null,
+email varchar(25) not null,
 chatID int not null,
 primary key (messageID),
-foreign key (psuID) references Account(psuID),
+foreign key (email) references Account(email),
 foreign key (chatID) references Chat(chatID));
 
-create table Information (
-infoID int not null,
-questions varchar(255),
-answers varchar(255),
-primary key (infoID));
+create table HelpDeskTickets (
+inforID int primary key,
+question text,
+response text,
+date date,
+category varchar(255)
+);
 
 create table AIResponse (
 responseID int not null,
 text varchar(255),
 timestamp timestamp,
 chatID int not null,
-infoID int not null,
+inforID int,
 primary key (responseID),
 foreign key (chatID) references Chat(chatID),
-foreign key (infoID) references Information(infoID));
+foreign key (inforID) references HelpDeskTickets(inforID));
