2e082b2ed16544fbad39c76533af54a6277f135f
TreasureAD <80182574+TreasureAD@users.noreply.github.com>
2023-10-31 09:43:53 -0400

    Add files via upload
    Website python file

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify
+
+
+import torch
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('chat2.html')
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+
+
+
+
+if __name__ == '__main__':
+    app.run()
